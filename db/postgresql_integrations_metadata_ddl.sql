eaf30655402f1dc66ad2c072f32944b1596e5965
sergio.ramazzina <sergio.ramazzina@serasoft.it>
2021-02-12 18:28:58 +0100

    Fixed integrations database entities names and characteristics

--- db/postgresql_integrations_metadata_ddl.sql
+++ db/postgresql_integrations_metadata_ddl.sql
@@ -3,28 +3,28 @@ drop user if exists integrations_user;
 
 CREATE USER integrations_user PASSWORD 'password';
 
-CREATE DATABASE integrations_metadata WITH OWNER = integration_user ENCODING = 'UTF8' TABLESPACE = pg_default;
+CREATE DATABASE integrations WITH OWNER = integration_user ENCODING = 'UTF8' TABLESPACE = pg_default;
 
-GRANT ALL PRIVILEGES ON DATABASE integrations_metadata to integrations_user;
+GRANT ALL PRIVILEGES ON DATABASE integrations to integrations_user;
 
-\connect integrations_metadata;
+\connect integrations;
 
 CREATE SCHEMA IF NOT EXISTS logs;
 
-CREATE TABLE logs.integration_log (
+CREATE TABLE logs.integrations_log (
 	id varchar(32) not null,
 	process varchar(32) NOT NULL,
 	proc_status char(1) NOT NULL,
 	max_gravity integer default(0),
-	date_started Datetime NOT NULL,
-	date_finished Datetime NULL,
+	date_started timestamp NOT NULL,
+	date_finished timestamp NULL,
 	log_file_ref varchar(255) NOT NULL,
 	username varchar(32) NULL,
 	host varchar(64) NULL,
 	CONSTRAINT integration_log_px PRIMARY KEY (id)
 );
 
-CREATE TABLE logs.integration_log_detail (
+CREATE TABLE logs.integrations_log_detail (
 	id varchar(32) not null,
 	integration_log_id varchar(32) not null,
 	source_sys_code varchar(32) NOT NULL,
@@ -39,12 +39,14 @@ CREATE SCHEMA IF NOT EXISTS master_data;
 
 CREATE TABLE master_data.geography (
 	id varchar(50) NOT NULL,
-	town varchar(100) NULL,
-	administrative_code varchar(50) NULL,
-	administrative_name varchar(100) NULL,
-	area varchar(50) NULL,
-	state varchar(50) NULL,
+	town varchar(100) NOT NULL,
+	administrative_code varchar(50) NOT NULL,
+	administrative_name varchar(100) NOT NULL,
+	area varchar(50) NOT NULL,
+	state varchar(50) NOT NULL,
 	population int4 NULL,
+	male int4 NULL,
+	female int4 NULL,
 	town_tax_id_code varchar(50) NULL,
 	CONSTRAINT pk_geography PRIMARY KEY (id)
 );
